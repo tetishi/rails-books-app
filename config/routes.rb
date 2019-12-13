194376b06b749f8235e8bab3854f97251a56c421
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2019-12-13 16:36:52 +0900

    i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :books
+  scope "(/:locale)", locale: /en|ja/ do
+    resources :books
+  end
+
+  get "/:locale" => "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
