f003f88a303fb958fe49bb44c065993a3b07a5c2
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2019-12-15 14:25:10 +0900

    add file and code for devise feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :set_locale
 
     private
@@ -8,5 +10,10 @@ class ApplicationController < ActionController::Base
 
     def default_url_options
         { locale: I18n.locale }
+    end
+
+    protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: [:zipcode, :address, :profile])
     end
 end
