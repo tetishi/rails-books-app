d4ffb85741fb9e95bf8f69a77d8dd2e4561c6c5a
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2019-12-15 14:25:10 +0900

    add file and code for devise feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  scope "(/:locale)", locale: /en|ja/ do
-    resources :books
+  get 'users/show'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
   end
 
-  get "/:locale" => "books#index"
+  resources :users, only: [:show, :index]
+  root "books#index"
+  resources :books
+
+  # scope "(/:locale)", locale: /en|ja/ do
+  #   resources :books
+  # end
+
+  # get "/:locale" => "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_084354) do
+ActiveRecord::Schema.define(version: 2019_12_15_043024) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2019_12_13_084354) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.text "profile"
+    t.string "zipcode"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
