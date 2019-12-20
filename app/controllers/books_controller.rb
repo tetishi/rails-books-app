55004aace24bb903c8e9d440ba378ad52fbf4657
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2019-12-20 14:00:46 +0900

    modified

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,25 +25,20 @@ class BooksController < ApplicationController
   # POST /books.json
   def create
     @book = Book.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: t("activerecord.attributes.book.created") }
-      else
-        format.html { render :new }
-      end
+    if @book.save
+      redirect_to @book, notice: t("activerecord.attributes.book.created")
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: t("activerecord.attributes.book.updated") }
-      else
-        format.html { render :edit }
-      end
+    if @book.update(book_params)
+      redirect_to @book, notice: t("activerecord.attributes.book.updated")
+    else
+      render :edit
     end
   end
 
@@ -51,9 +46,7 @@ class BooksController < ApplicationController
   # DELETE /books/1.json
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: t("activerecord.attributes.book.destroyed") }
-    end
+    redirect_to books_url, notice: t("activerecord.attributes.book.destroyed")
   end
 
   private
