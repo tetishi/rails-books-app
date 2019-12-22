6081bb0346de07c4555222c63afc5bfd93b2b34d
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2019-12-22 17:08:41 +0900

    fixed an error about an unfollowing button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,20 +4,22 @@ class FriendshipsController < ApplicationController
   before_action :find_user
 
   def create
-    @user = User.find(params[:followed_id])
+    # @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    redirect_to @user
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @user = Relationship.find_by(followed_id: params[:followed_id]).followed
     current_user.unfollow(@user)
-    redirect_to @user
+    redirect_back(fallback_location: root_path)
+    # render template: '/friendships/:id'
   end
 
   private
 
   def find_user
+    # @user = User.find(params[:user_id])
+    # @usr = User.find(params[:id])
     @user = User.find(params[:user_id])
   end
 end
