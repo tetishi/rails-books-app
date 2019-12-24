43f6e9a771b8bf0d745cb2e169d4656362945020
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2019-12-24 13:17:36 +0900

    deleted comments

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,7 +4,6 @@ class FriendshipsController < ApplicationController
   before_action :find_user
 
   def create
-    # @user = User.find(params[:followed_id])
     current_user.follow(@user)
     redirect_back(fallback_location: root_path)
   end
@@ -12,14 +11,11 @@ class FriendshipsController < ApplicationController
   def destroy
     current_user.unfollow(@user)
     redirect_back(fallback_location: root_path)
-    # render template: '/friendships/:id'
   end
 
   private
 
   def find_user
-    # @user = User.find(params[:user_id])
-    # @usr = User.find(params[:id])
     @user = User.find(params[:user_id])
   end
 end
