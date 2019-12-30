89591d1b966c513d3185c6ccde4491b72b6a738e
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2019-12-30 15:27:44 +0900

    added test code

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
+  setup do
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+  end
+
+  test 'user1 は user2 をフォローする' do
+    @user1.follow(users(:user2))
+    assert @user1.following.include?(users(:user2))
+  end
+
+  # test 'create course' do
+  #   login_user 'ishida', 'testtest'
+  #   visit '/courses/new'
   #   assert true
   # end
 end
