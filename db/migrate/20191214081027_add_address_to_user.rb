ac627dbc914c51476bb87bb45107999025fb63de
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-01-01 16:45:56 +0900

    adjusted file

--- db/migrate/20191214081027_add_address_to_user.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddAddressToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :address, :string
-  end
-end
--- db/migrate/20191214081104_add_profile_to_user.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddProfileToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :profile, :text
-  end
-end
