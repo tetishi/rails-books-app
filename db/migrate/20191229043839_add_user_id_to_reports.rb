a13cc38e066242808c6f1862300c27576a20367d
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-01-10 01:36:07 +0900

    deleted unnecessary comment

--- db/migrate/20191229043839_add_user_id_to_reports.rb
+++ db/migrate/20191229043839_add_user_id_to_reports.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
 class AddUserIdToReports < ActiveRecord::Migration[6.0]
-  # def up
-  #   add_column :reports, :user_id, :integer
-  #   change_column :reports, :user_id, :integer
-  # end
-
-  # def down
-  #   change_column :reports, :user_id, :integer
-  # end
-
   def change_column
     add_column :reports, :user_id, :integer
   end
